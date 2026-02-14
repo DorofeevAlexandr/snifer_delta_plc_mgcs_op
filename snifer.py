75a84c9cedf88ba9abcda299bbf1942f0f643e66
DorofeevAlexandr <amd_86@mail.ru>
2026-02-14 17:53:24 +0300

    Реализовано чтение данных с порта панели оператора

--- snifer.py
+++ snifer.py
@@ -1,6 +1,46 @@
+import keyboard
 import serial
 
-ser = serial.Serial(port='COM1', baudrate=9600, timeout=1)
-ser.write(b'command')
-data = ser.readline()
-ser.close()
+
+class Snifer:    
+    def __init__(self,
+                 port='COM1',
+                 baudrate=9600, 
+                 bytesize=7,
+                 parity='N',
+                 stopbits=1,
+                 timeout=1):
+        
+        self.ser = serial.Serial(port=port, 
+                                baudrate=baudrate, 
+                                bytesize=bytesize,
+                                parity=parity,
+                                stopbits=stopbits,
+                                timeout=timeout)
+        print(self.ser)
+        self.requests = set()
+
+    # ser.write(b'command')
+    def read_port(self):
+        self.data = self.ser.readline()
+        if not(self.data in self.requests):
+            self.requests.add(self.data)
+            result = [self.data[i:i+1] for i in range(0, len(self.data))]
+            print(self.data)
+            print(result)
+            return result
+
+    def port_close(self):
+        self.ser.close()
+
+
+if __name__ == '__main__':
+    panel_op = Snifer()
+    
+    print('')
+    print('Для остановки нажмите кнопку "escape"')
+    while True:
+            new_request = panel_op.read_port()
+            if keyboard.is_pressed("escape"):
+                print("Выход")
+                break
